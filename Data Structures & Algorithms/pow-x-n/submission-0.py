6e7322dfc6e9edbc8510bf1366fc68b85346fbc2
akashew32 <ajha17@sas.upenn.edu>
2026-07-30 06:54:49 -0400

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n == -1:
+            return 1.0 / x
+        elif n == 1:
+            return x
+        elif n % 2:
+            temp = self.myPow(x, n // 2)
+            return x * temp * temp
+        else:
+            temp = self.myPow(x, n // 2)
+            return temp * temp
+
+        
